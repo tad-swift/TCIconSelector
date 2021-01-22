d7a5715f86c9415330a3a57851289179357bb9be
Tadreik Campbell <tadreik@gmail.com>
2021-01-22 13:44:47 -0500

    Initial Commit

---
+++ TC Icon Selector/TC_Icon_Selector.h
@@ -0,0 +1,18 @@
+//
+//  TC_Icon_Selector.h
+//  TC Icon Selector
+//
+//  Created by Tadreik Campbell on 1/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TC_Icon_Selector.
+FOUNDATION_EXPORT double TC_Icon_SelectorVersionNumber;
+
+//! Project version string for TC_Icon_Selector.
+FOUNDATION_EXPORT const unsigned char TC_Icon_SelectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TC_Icon_Selector/PublicHeader.h>
+
+
